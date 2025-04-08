985393b1306a56fd33ca57767afa9daca23c797e
nexxooo <fabiolongofabio@gmail.com>
2025-04-08 17:24:29 +0200

    avancer

--- gameoflife.py
+++ gameoflife.py
@@ -1,13 +1,15 @@
 from random import * 
 class Game:
-    def __init__(self,h,l):
+    def __init__(self,h,l,t):
         self.hauteur = h
         self.largeur = l
-        self.tab = self.random_state()
+        self.tab = t 
     
     def dead_state(self):
         res = [[0] * self.largeur for _ in range(self.hauteur)]
-        return res
+        self.tab = res
+
+    
     def random_state(self):
         tab = self.dead_state()
         for i in tab:
@@ -26,8 +28,44 @@ class Game:
                     ch+= "■ "    
             res += ch + "\n"    
         return res     
+    
+    def avancer(self):
+        direction = [[-1,-1],[0,-1],[1,-1],[-1,0],[1,0],[-1,1],[0,1],[1,1]]
+        res = [[0] * self.largeur for _ in range(self.hauteur)]
+        for ligne in range(len(self.tab)):
+            for colone in range(len(self.tab[ligne])):
+                nombre_voisin = 0
+                for dx, dy in direction:
+                    ni = ligne + dx
+                    nj = colone + dy
+                    if 0 <= ni < self.hauteur and 0 <= nj < self.largeur:
+                        nombre_voisin += self.tab[ni][nj]
+
+                if self.tab[ligne][colone] == 1:
+                    if nombre_voisin in [0,1]:
+                        res[ligne][colone]= 0
+                    elif nombre_voisin in [2,3]:
+                        res[ligne][colone]=1
+                    elif nombre_voisin > 3:
+                        res[ligne][colone] = 0
+                else:
+                    if nombre_voisin ==3:
+                        res[ligne][colone] = 1
+        self.tab = res
+
+
+                     
+
+
 
 
 #print(dead_state(2,2))
-test = Game(5,5)
+test = Game(3,3,[
+        [0,0,1],
+        [0,1,1],
+        [0,0,0]
+    ])
+print(test)
+test.avancer()
+print("__________________")
 print(test)
